bb2058b95ccba3626d4c5f65f1ac7142f5d1d491
Satanarious <satyam048niranjan@gmail.com>
2024-05-04 20:19:48 +0530

    Fix bard bug and general styling

--- background.js
+++ background.js
@@ -1,132 +1,165 @@
 // Ignore X-Frame-Options header - Original code from: https://gist.github.com/dergachev/e216b25d9a144914eae2
 chrome.webRequest.onHeadersReceived.addListener(
-    function(details) {
-        for (var i = 0; i < details.responseHeaders.length; ++i) {
-            if (details.responseHeaders[i].name.toLowerCase() == 'x-frame-options') {
-                details.responseHeaders.splice(i, 1);
-                return {
-                    responseHeaders: details.responseHeaders
-                };
-            }
-        }
-    }, {
-        urls: ["*://www.bing.com/*", "*://www.google.com/*", "*://www.google.co.in/*"],
-        types: [ 'sub_frame' ]
-    }, ["blocking", "responseHeaders", "extraHeaders"]
+  function (details) {
+    for (var i = 0; i < details.responseHeaders.length; ++i) {
+      if (details.responseHeaders[i].name.toLowerCase() == "x-frame-options") {
+        details.responseHeaders.splice(i, 1);
+        return {
+          responseHeaders: details.responseHeaders,
+        };
+      }
+    }
+  },
+  {
+    urls: [
+      "*://www.bing.com/*",
+      "*://www.google.com/*",
+      "*://www.google.co.in/*",
+    ],
+    types: ["sub_frame"],
+  },
+  ["blocking", "responseHeaders", "extraHeaders"]
 );
 
 // Update request headers
 chrome.webRequest.onBeforeSendHeaders.addListener(
-    changeRequestHeaders,
-    {
-        urls: ["*://www.bing.com/*", "*://www.google.com/*", "*://www.google.co.in/*"],
-    },
-    ['blocking', 'requestHeaders', 'extraHeaders']
+  changeRequestHeaders,
+  {
+    urls: [
+      "*://www.bing.com/*",
+      "*://www.google.com/*",
+      "*://www.google.co.in/*",
+    ],
+  },
+  ["blocking", "requestHeaders", "extraHeaders"]
 );
 
 // Add Edge as a user agent to support Bing chat
 function changeRequestHeaders(details) {
-    for (var i = 0; i < details.requestHeaders.length; ++i) {
-        if (
-            details.requestHeaders[i].name.toLowerCase() === 'user-agent'
-            && details.url.indexOf('Edg') <= -1
-        ) {
-            details.requestHeaders[i].value = details.requestHeaders[i].value + ' Edg/111.0.1661.44';
-        }
-
-        if (
-            details.requestHeaders[i].name.toLowerCase() === 'sec-ch-ua'
-            && details.url.indexOf('Google Chrome') <= -1
-        ) {
-            let value = details.requestHeaders[i].value;
-            let valueList = value.split(', ');
-            valueList[valueList.length - 1] = '"Microsoft Edge";v="114"'
-            value = valueList.join(', ')
-            details.requestHeaders[i].value = value;
-        }
+  for (var i = 0; i < details.requestHeaders.length; ++i) {
+    if (
+      details.requestHeaders[i].name.toLowerCase() === "user-agent" &&
+      details.url.indexOf("Edg") <= -1
+    ) {
+      details.requestHeaders[i].value =
+        details.requestHeaders[i].value + " Edg/111.0.1661.44";
     }
-      
-    return {
-        requestHeaders: details.requestHeaders,
-    };
+
+    if (
+      details.requestHeaders[i].name.toLowerCase() === "sec-ch-ua" &&
+      details.url.indexOf("Google Chrome") <= -1
+    ) {
+      let value = details.requestHeaders[i].value;
+      let valueList = value.split(", ");
+      valueList[valueList.length - 1] = '"Microsoft Edge";v="114"';
+      value = valueList.join(", ");
+      details.requestHeaders[i].value = value;
+    }
+  }
+
+  return {
+    requestHeaders: details.requestHeaders,
+  };
 }
 
 // !! https://developer.chrome.com/extensions/settings_override
 //urls: ["*://www.bing.com/*", "*://www.google.com/*"]
 
 // This event is fired with the user accepts the input in the omnibox.
-chrome.omnibox.onInputEntered.addListener(function(query) {
-    if (query.startsWith('chat ')) {
-        var searchUrl = `https://www.bing.com/search?q=${encodeURIComponent(query.substring(5))}&showconv=1&sendquery=1&FORM=hpcodx`;
-        var bardUrl = 'https://bard.google.com/';
-        chrome.tabs.create({ url: bardUrl }, function(tab) {
-            var bardTabId = tab.id;
-            chrome.tabs.executeScript(bardTabId, { code: 'var iframe = document.createElement("iframe");' +
-                                                       'iframe.src = "' + searchUrl + '";' +
-                                                       'iframe.style = "width:50%;height:100%;position:absolute;top:0;right:0;z-index:99999";' +
-                                                       'document.body.appendChild(iframe);'
-            });
-        });
-    } else {
-        var search_url = chrome.runtime.getURL("search.html");
-        search_url = search_url + "?q=" + encodeURIComponent(query);
-        navigate(search_url);
-    }
+chrome.omnibox.onInputEntered.addListener(function (query) {
+  if (query.startsWith("chat ")) {
+    var searchUrl = `https://www.bing.com/search?q=${encodeURIComponent(
+      query.substring(5)
+    )}&showconv=1&sendquery=1&FORM=hpcodx`;
+    var bardUrl = "https://bard.google.com/";
+    chrome.tabs.create({ url: bardUrl }, function (tab) {
+      var bardTabId = tab.id;
+      chrome.tabs.executeScript(bardTabId, {
+        code:
+          'var iframe = document.createElement("iframe");' +
+          'iframe.src = "' +
+          searchUrl +
+          '";' +
+          'iframe.style = "width:50%;height:100%;position:absolute;top:0;right:0;z-index:99999";' +
+          "document.body.appendChild(iframe);",
+      });
+    });
+  } else {
+    var search_url = chrome.runtime.getURL("search.html");
+    search_url = search_url + "?q=" + encodeURIComponent(query);
+    navigate(search_url);
+  }
 });
 
 function navigate(url) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.update(tabs[0].id, {url: url});
-    });
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.update(tabs[0].id, { url: url });
+  });
 }
 
 const shopPrefix = "shop";
 const chatPrefix = "chat";
 
 chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        if (details.url.indexOf("https://www.doubleshotsearch.download/chrome-extension/search/?q=") !== -1
-            || details.url.indexOf("chrome-extension://" + chrome.runtime.id + "/?q=") !== -1) {
-            var url = $.url(details.url);
-            var query = url.param("q");
-            var search_url = chrome.runtime.getURL("search.html");
-            search_url = search_url + "?q=" + encodeURIComponent(query);
+  function (details) {
+    if (
+      details.url.indexOf(
+        "https://www.doubleshotsearch.download/chrome-extension/search/?q="
+      ) !== -1 ||
+      details.url.indexOf(
+        "chrome-extension://" + chrome.runtime.id + "/?q="
+      ) !== -1
+    ) {
+      var url = $.url(details.url);
+      var query = url.param("q");
+      var search_url = chrome.runtime.getURL("search.html");
+      search_url = search_url + "?q=" + encodeURIComponent(query);
 
-            let queryWords = query.split(" ");
-            if (queryWords.length > 1) {
-                switch (queryWords[0].toLowerCase()) {
-                    case shopPrefix:
-                        queryWords[0] = shopPrefix;
-                        query = queryWords.join(" ");
-                        break;
-                    case chatPrefix:
-                        queryWords[0] = chatPrefix;
-                        query = queryWords.join(" ");
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            if (query.startsWith("chat ")) {
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    chrome.tabs.update(tabs[0].id, { url: "https://bard.google.com/" }, function () {
-                        setTimeout(() => {
-                            chrome.tabs.sendMessage(tabs[0].id, {
-                                action: "chat",
-                                chatText: query.substring(5),
-                            });
-                        }, 2000);
-                    });
-                });
-            } else {
-                // Update to support the Edge version: 111.0.1661.54
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    chrome.tabs.update(tabs[0].id, { url: search_url });
-                });
-            }
+      let queryWords = query.split(" ");
+      if (queryWords.length > 1) {
+        switch (queryWords[0].toLowerCase()) {
+          case shopPrefix:
+            queryWords[0] = shopPrefix;
+            query = queryWords.join(" ");
+            break;
+          case chatPrefix:
+            queryWords[0] = chatPrefix;
+            query = queryWords.join(" ");
+            break;
+          default:
+            break;
         }
-    },
-    {urls: ["<all_urls>"]},
-    ["blocking"]
+      }
+
+      if (query.startsWith("chat ")) {
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            chrome.tabs.update(
+              tabs[0].id,
+              { url: "https://bard.google.com/" },
+              function () {
+                setTimeout(() => {
+                  chrome.tabs.sendMessage(tabs[0].id, {
+                    action: "chat",
+                    chatText: query.substring(5),
+                  });
+                }, 2000);
+              }
+            );
+          }
+        );
+      } else {
+        // Update to support the Edge version: 111.0.1661.54
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            chrome.tabs.update(tabs[0].id, { url: search_url });
+          }
+        );
+      }
+    }
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
 );
--- bard.js
+++ bard.js
@@ -1,18 +1,19 @@
+// resize the chat and add Bing Chat under an iframe
+var chatApp = document.querySelector("chat-app");
+chatApp.style = "width:50%";
+var header = document.querySelector("header");
+header.style = "width:50%";
+
+var iframe = document.createElement("iframe");
+iframe.style =
+  "width:50%;height:100%;position:absolute;top:0;right:0;z-index:99999";
+
+document.body.appendChild(iframe);
+
 // listen to chat request from background.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "chat") {
     const chatText = request.chatText;
-    // resize the chat and add Bing Chat under an iframe
-    var chatApp = document.querySelector('chat-app')
-    chatApp.style = "width:50%";
-    var header = document.querySelector('header')
-    header.style = "width:50%";
-
-    var iframe = document.createElement("iframe");
-    iframe.style = "width:50%;height:100%;position:absolute;top:0;right:0";
-
-    document.body.appendChild(iframe); 
-        
 
     const waitForElement = (selector, callback) => {
       const element = document.querySelector(selector);
@@ -25,17 +26,28 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       }
     };
 
-    waitForElement('div.ql-editor.textarea > p', (textarea) => {
-      textarea.append(chatText)
-      waitForElement("button[aria-label='Send message']", (button) => {
-        // we need setTimeout because the button got rendered before textarea.
-        setTimeout(() => {
+    waitForElement(
+      'textarea[placeholder="Enter a prompt here"]',
+      (textarea) => {
+        textarea.value = chatText;
+
+        // Trigger the input event
+        const inputEvent = new Event("input", {
+          bubbles: true,
+          cancelable: true,
+        });
+        textarea.dispatchEvent(inputEvent);
+
+        const button = document.querySelector(
+          'button[aria-label="Send message"]'
+        );
+        if (button && !button.disabled) {
           button.click();
-        }, 10);
-      });
-    });
+        }
+      }
+    );
 
     var searchUrl = `https://www.bing.com/search?q=${chatText}&showconv=1&sendquery=1&FORM=hpcodx`;
-    iframe.src = searchUrl
+    iframe.src = searchUrl;
   }
 });
